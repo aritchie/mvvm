a3e18182a66d07b6f050defbd48aeeb10a325f4f
Allan Ritchie <allan.ritchie@gmail.com>
2015-05-28 13:26:53 -0400

    Minor updates

---
+++ Acr.XamForms/Converters/ColorConverter.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Globalization;
+//using C = System.Drawing.Color;
+//using Xamarin.Forms;
+
+
+//namespace Acr.XamForms {
+
+//    public class ColorConverter : IValueConverter {
+
+//        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+//            var color = (C)value;
+//            return Color.FromRgba(color.R, color.G, color.B, color.A);
+//        }
+
+
+//        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+//            var color = (Color)value;
+//            return C.FromArgb((int)color.A, (int)color.R, (int)color.G, (int)color.B);
+//        }
+//    }
+//}
---
+++ Acr.XamForms/Converters/SplatImageConverter.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Globalization;
+//using System.Threading;
+//using Splat;
+//using Xamarin.Forms;
+
+
+//namespace Acr.XamForms {
+
+//    public class SplatImageConverter : IValueConverter {
+
+//        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+//            var bitmap = value as IBitmap;
+//            if (bitmap == null)
+//                throw new ArgumentException("");
+
+//            return ImageSource.FromStream(() => bitmap);
+//        }
+
+
+//        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+//            var file = value as FileImageSource;
+//            if (file != null)
+//                return BitmapLoader.Current.Load(file.File);
+
+//            var res = value as StreamImageSource;
+//            if (res != null) {
+//                var stream = res.Stream(CancellationToken.None).Result;
+//                return BitmapLoader.Current.Load(stream).Result;
+//            }
+
+//            return null;
+//        }
+//    }
+//}
